b8d069a664017c37743976a1e81830272d9430bb
VJoshi380 <16640446+VJoshi380@users.noreply.github.com>
2019-01-19 09:43:07 -0800

    Experimental v.1.1
    Added encoder support. Requires testing before implementation

--- SparkControllerFactory.java
+++ SparkControllerFactory.java
@@ -1,18 +1,22 @@
-package com.team3925.utils;
+
 
 
 import com.revrobotics.*;
+import com.revrobotics.CANDigitalInput.LimitSwitchPolarity;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class SparkControllerFactory {
-    public static CANSparkMax createDefaultSparkMax(int id,MotorType mType){
+    public static CANSparkMax createDefaultSparkMax(int id,MotorType mType, CANEncoder encoder){
         CANSparkMax sparkmax = new CANSparkMax(id,mType );
+            sparkmax.getEncoder();
             return sparkmax;
     }
     public static CANSparkMax createFollower(int id, MotorType mType,CANSparkMax leader){
         CANSparkMax sparkmax = new CANSparkMax(id, mType);
         sparkmax.follow(leader);
         return sparkmax;
+
             
         
     }
